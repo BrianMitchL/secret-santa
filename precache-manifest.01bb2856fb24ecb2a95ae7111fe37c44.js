3fb6b885d87e664c08dc418303d323d30d6aeaae
Brian Mitchell <bman4789@gmail.com>
2019-09-23 01:32:05 -0500

    Updates

---
+++ precache-manifest.01bb2856fb24ecb2a95ae7111fe37c44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e58ae5f00757bd5eca283a277575c1d",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "3b2e370ffd4972a023db",
+    "url": "/secret-santa/static/css/main.0dcd856f.chunk.css"
+  },
+  {
+    "revision": "5b3b74bb92a306ae36da",
+    "url": "/secret-santa/static/js/2.7059e793.chunk.js"
+  },
+  {
+    "revision": "3b2e370ffd4972a023db",
+    "url": "/secret-santa/static/js/main.1b42d624.chunk.js"
+  },
+  {
+    "revision": "59a1f9a7d6b4b70ccf2f",
+    "url": "/secret-santa/static/js/runtime-main.4ace76f2.js"
+  }
+]);
