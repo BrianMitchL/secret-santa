409c06a1f9d4ecc8bc52603dccada40189ba97eb
Brian Mitchell <bman4789@gmail.com>
2025-02-06 17:02:11 -0600

    Upgrade to latest React and Vite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,5 @@
-import ReactDOM from 'react-dom';
 import App from './App';
 import { render, screen } from '@testing-library/react';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
 it('renders a random santa emoji and sets the document title', () => {
   render(<App />);
